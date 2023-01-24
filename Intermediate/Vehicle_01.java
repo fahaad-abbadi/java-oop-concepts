eb19b8c4813292fd24eed8918513c7d78e5f5ce2
fahaad-abbadi <al.fahaadabbadi@gmail.com>
2023-01-25 00:29:56 +0530

    initial commit

--- Intermediate/Vehicle_01.java
+++ Intermediate/Vehicle_01.java
@@ -1,5 +1,28 @@
 package Intermediate;
 
 public class Vehicle_01 {
-    
+    int no_of_wheels;
+    int max_speed;
+    int fuel_capacity;
+
+    boolean isGear;
+
+
+    //using a parameterized constructor
+    public Vehicle_01(int no_of_wheels, int max_speed, boolean isGear)
+    {
+        this.no_of_wheels = no_of_wheels;
+        this.max_speed = max_speed;
+        this.isGear = isGear;
+    }
+
+    public void accelarate()
+    {
+        System.out.println("The vehicle is speeding up...");
+    }
+
+    public void stop()
+    {
+        System.out.println("Stopping...");
+    }
 }
