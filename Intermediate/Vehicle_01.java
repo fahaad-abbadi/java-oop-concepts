3fed4ca40f651200c6fdaccc0dbf69a320bc8465
fahaad-abbadi <al.fahaadabbadi@gmail.com>
2023-01-25 00:39:06 +0530

    extended class

--- Intermediate/Vehicle_01.java
+++ Intermediate/Vehicle_01.java
@@ -7,6 +7,11 @@ public class Vehicle_01 {
 
     boolean isGear;
 
+    public Vehicle_01()
+    {
+        super();
+    }
+
 
     //using a parameterized constructor
     public Vehicle_01(int no_of_wheels, int max_speed, boolean isGear)
@@ -16,6 +21,7 @@ public class Vehicle_01 {
         this.isGear = isGear;
     }
 
+    //the below ones are methods
     public void accelarate()
     {
         System.out.println("The vehicle is speeding up...");
@@ -24,5 +30,19 @@ public class Vehicle_01 {
     public void stop()
     {
         System.out.println("Stopping...");
+    }
+    public static void main(String[] args) {
+        Vehicle_01 v = new Vehicle_01(0, 0, false);
+        v.isGear = true;
+    }
+}
+
+
+class Car extends Vehicle_01{
+    boolean isElectric;
+
+    Car()
+    {
+        super();
     }
 }
